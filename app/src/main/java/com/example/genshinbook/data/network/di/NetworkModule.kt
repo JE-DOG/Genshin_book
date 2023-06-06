dcaf891a0315c4efbffdc3aa9f5ac77879e1705c
JE_DOG <gamzat.magomedov809@gmail.com>
2023-06-06 14:54:04 +0300

    Remove Network object and make network and api by di

--- app/src/main/java/com/example/genshinbook/data/network/di/NetworkModule.kt
+++ app/src/main/java/com/example/genshinbook/data/network/di/NetworkModule.kt
@@ -2,6 +2,11 @@ package com.example.genshinbook.data.network.di
 
 import com.example.genshinbook.data.network.repository.characters.di.CharacterNetworkModule
 import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+private const val BASE_URL = "https://api.genshin.dev/"
 
 @Module(
     includes = [
@@ -9,7 +14,13 @@ import dagger.Module
     ]
 )
 class NetworkModule{
-
-    //todo provide retrofit
+    @Provides
+    fun provideRetrofit(): Retrofit {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        return retrofit
+    }
 
 }
--- app/src/main/java/com/example/genshinbook/data/network/repository/characters/di/CharacterNetworkModule.kt
+++ app/src/main/java/com/example/genshinbook/data/network/repository/characters/di/CharacterNetworkModule.kt
@@ -1,18 +1,29 @@
 package com.example.genshinbook.data.network.repository.characters.di
 
+import com.example.genshinbook.data.network.api.CharactersApi
 import com.example.genshinbook.data.network.repository.characters.CharactersNetwork
 import com.example.genshinbook.data.network.repository.characters.CharactersNetworkImpl
 import dagger.Module
 import dagger.Provides
+import retrofit2.Retrofit
 
 @Module
 class CharacterNetworkModule {
 
     //todo provide character network repo
+    @Provides
+    fun provideCharacterApi(
+        retrofit: Retrofit
+    ): CharactersApi{
+        val characterApi = retrofit.create(CharactersApi::class.java)
+        return characterApi
+    }
 
     @Provides
-    fun provideCharactersNetwork(): CharactersNetwork {
-        return CharactersNetworkImpl()
+    fun provideCharactersNetwork(
+        charactersApi: CharactersApi
+    ): CharactersNetwork {
+        return CharactersNetworkImpl(charactersApi)
     }
 
 }
