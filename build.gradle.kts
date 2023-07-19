db35bad56f2d365b768e74bb4a63586b962faaa4
JE_DOG <gamzat.magomedov809@gmail.com>
2023-07-19 11:14:21 +0300

    Main change: 1.Convert gradle dsl to kotlin dsl 2.Add buildSrc for dependencies

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id ("com.android.application") version "8.0.0" apply false
+    id ("com.android.library") version "8.0.0" apply false
+    id ("org.jetbrains.kotlin.android") version "1.8.10" apply false
+    //realm
+    id ("io.realm.kotlin") version "1.6.1" apply false
+}
