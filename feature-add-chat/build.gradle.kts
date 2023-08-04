dcbbc0c8d8aa9d2d70aefcbc563866dae08354bf
JE_DOG <gamzat.magomedov809@gmail.com>
2023-08-04 17:17:58 +0300

    Main change: Create module feature-add-chat

---
+++ feature-add-chat/build.gradle.kts
@@ -0,0 +1,39 @@
+plugins {
+    id(Plugins.Android.library)
+    id(Plugins.Kotlin.android)
+}
+
+android {
+    namespace = "com.example.feature.add.chat"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 29
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    api (Modules.Feature.core)
+
+}
